0d732f944873a5982c31f912418c4fd183c602d3
Matheus Burthon <matatiaspatrizio@hotmail.com>
2022-09-03 16:46:43 -0300

    feat: :zap: fanzendo a funcionalidade de pesquisar na API do core, no APP.
    setando estados para colocar dados da API no frontend, utilizando hook useState para armazenar estados, removendo arquivo que foi denecessário.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import './App.css';
-// import env from 'react-dotenv';
+import env from 'react-dotenv';
+// agora vamos testar se as api consegue retornar dados na aplicação
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [responseApi, setResponseApi] = useState([]);
+
+  const searchScienceArticles = async (searchValue) => {
+    const url = `https://core.ac.uk:443/api-v2/search/${searchValue}?page=1&pageSize=10&apiKey=${env.API_KEY}`;
+    const request = await fetch(url);
+    const { data } = await request.json();
+    setResponseApi(data);
+  };
+
   return (
     <div className="App">
+      {window.console.log(responseApi)}
       <h1>Aplicação para mostrar pesquisas cientificas.</h1>
+      <input
+        type="text"
+        placeholder="ache seu artigo"
+        onChange={({ target }) => setSearch(target.value)}
+      />
       <Button
         variant="contained"
         color="success"
-        onClick={() => window.console.log('versão do react-scripts, atualizada.')}
+        onClick={() => searchScienceArticles(search)}
       >
         Contained
       </Button>
