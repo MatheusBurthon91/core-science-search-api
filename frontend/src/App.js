153363d97c2f014d81f68790dd9ea7d6d710d676
Matheus Burthon <matatiaspatrizio@hotmail.com>
2022-09-06 00:56:51 -0300

    feat: :sparkles: adição me mais uma rota para tratamento de possiveis erros.
    caso uma rota não seja encontrada ela vai direcionar para uma pagina padrão, é um tratamento para possiveis erros.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,16 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
 import HistorySearch from './pages/HistorySearch';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Switch>
-      <Route path="/" component={Home} exact />
-      <Route path="/favorites" component={Favorites} exact />
-      <Route path="/search_history" component={HistorySearch} exact />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/favorites" component={Favorites} />
+      <Route exact path="/search_history" component={HistorySearch} />
+      <Route exact path="*" component={NotFound} />
     </Switch>
-
   );
 }
 
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <main>
+      <h1>Você se perdeu retorne para a pagina inicial.</h1>
+    </main>
+  );
+}
