8807c381b38ef97545f41e82104165fbb27f8021
Matheus Burthon <matatiaspatrizio@hotmail.com>
2022-09-13 23:39:24 -0300

    style: :lipstick: adicionando uma borda e margin no componente showItens para deixa-lo mais visual.

--- frontend/src/components/ShowItens.jsx
+++ frontend/src/components/ShowItens.jsx
@@ -9,33 +9,34 @@ import handleTitleForRender from '../helpers/HandleTitle';
 
 export default function ShowItens({ res, ind }) {
   return (
-    <Grid key={res._id} id={ind + res._id} container justifyContent="center" alignItems="center" sx={{ backgroundColor: '#AFB4FF' }}>
-      <Grid container justifyContent="center" alignItems="center">
-        <Typography sx={{ fontSize: 25, color: 'blue' }} variant="h1" component="div" gutterBottom>
-          <Typography sx={{ fontSize: 25, fontWeight: 'bold' }} variant="span">
-            Autores:
+    <Grid key={res._id} id={ind + res._id} container justifyContent="center" alignItems="center" sx={{ backgroundColor: '#AFB4FF', border: '1px solid white' }}>
+      <Grid container justifyContent="center" alignItems="center" sx={{ margin: '15px' }}>
+        <Grid container justifyContent="center" alignItems="center">
+          <Typography sx={{ fontSize: 25, color: 'blue' }} variant="h1" component="div" gutterBottom>
+            <Typography sx={{ fontSize: 25, fontWeight: 'bold' }} variant="span">
+              Autores:
+            </Typography>
+            {handleTitleForRender(res._source.authors)}
           </Typography>
-          {handleTitleForRender(res._source.authors)}
-        </Typography>
-      </Grid>
-      <Grid container justifyContent="center" alignItems="center">
-        <Typography sx={{ fontSize: 19 }} variant="h2" gutterBottom>
-          <Typography sx={{ fontSize: 19, fontWeight: 'bold' }} variant="span">
-            titulo do artigo:
+        </Grid>
+        <Grid container justifyContent="center" alignItems="center">
+          <Typography sx={{ fontSize: 19 }} variant="h2" gutterBottom>
+            <Typography sx={{ fontSize: 19, fontWeight: 'bold' }} variant="span">
+              titulo do artigo:
+            </Typography>
+            {` ${res._source.title}`}
           </Typography>
-          {` ${res._source.title}`}
-        </Typography>
-      </Grid>
-      <Grid container justifyContent="center" alignItems="center">
-        <Typography sx={{ fontSize: 17 }} variant="h3" component="div" gutterBottom>
-          <Typography sx={{ fontSize: 17, fontWeight: 'bold' }} variant="span">
-            tipo de dado:
+        </Grid>
+        <Grid container justifyContent="center" alignItems="center">
+          <Typography sx={{ fontSize: 17 }} variant="h3" component="div" gutterBottom>
+            <Typography sx={{ fontSize: 17, fontWeight: 'bold' }} variant="span">
+              tipo de dado:
+            </Typography>
+            {` ${res._type}`}
           </Typography>
-          {` ${res._type}`}
-        </Typography>
-      </Grid>
-      <Grid container justifyContent="center" alignItems="center">
-        {
+        </Grid>
+        <Grid container justifyContent="center" alignItems="center">
+          {
             !res._source.description ? (
               <Typography sx={{ fontSize: 17 }} variant="p" component="div" gutterBottom>
                 <Typography sx={{ fontSize: 17, fontWeight: 'bold' }} variant="span">
@@ -52,67 +53,69 @@ export default function ShowItens({ res, ind }) {
               </Typography>
             )
         }
-      </Grid>
-      <Grid container justifyContent="center" alignItems="center">
-        <Grid container justifyContent="center" alignItems="center">
-          <Typography sx={{ fontSize: 17, fontWeight: 'bold' }} variant="h3" component="div" gutterBottom>
-            mais informações:
-          </Typography>
         </Grid>
-        { !res._source.urls.length ? (
-          <Typography variant="h6" component="div" gutterBottom>
-            sem links
-          </Typography>
-        )
-          : res._source.urls.map((url, index) => (
-            <List key={Math.random()}>
-              <ListItem disablePadding>
-                <Grid
-                  container
-                  item
-                  xs={5}
-                  md={3}
-                  direction="column"
-                >
-                  <Button variant="contained" color="success" size="small">
-                    <ListItemButton
-                      component="a"
-                      href={url}
-                      target="_blank"
-                      rel="noreferrer"
-                      underline="hover"
-                      alignItems="center"
-                    >
-                      <Typography sx={{ fontSize: 10 }} variant="span" component="div">
-                        {` link: ${index + 1}`}
-                      </Typography>
-                    </ListItemButton>
-                  </Button>
-                </Grid>
-              </ListItem>
-            </List>
-          ))}
-      </Grid>
-      <Grid container justifyContent="center" alignItems="center" sx={{ marginBottom: '13px', marginTop: '13px' }}>
-        <Button
-          type="button"
-          onClick={() => {
-            const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-            const findSomeFavorite = favorites.some((favorite) => favorite._id === res._id);
-            if (findSomeFavorite) {
-              const filteredFavorites = favorites.filter(({ _id }) => _id !== res._id);
-              localStorage.setItem('favorites', JSON.stringify(filteredFavorites));
-            } else {
-              saveHistorySearch('favorites', res);
-            }
-          }}
-          size="small"
-          variant="contained"
-        >
-          <FavoriteIcon />
-        </Button>
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid container justifyContent="center" alignItems="center">
+            <Typography sx={{ fontSize: 17, fontWeight: 'bold' }} variant="h3" component="div" gutterBottom>
+              mais informações:
+            </Typography>
+          </Grid>
+          { !res._source.urls.length ? (
+            <Typography variant="h6" component="div" gutterBottom>
+              sem links
+            </Typography>
+          )
+            : res._source.urls.map((url, index) => (
+              <List key={Math.random()}>
+                <ListItem disablePadding>
+                  <Grid
+                    container
+                    item
+                    xs={5}
+                    md={3}
+                    direction="column"
+                  >
+                    <Button variant="contained" color="success" size="small">
+                      <ListItemButton
+                        component="a"
+                        href={url}
+                        target="_blank"
+                        rel="noreferrer"
+                        underline="hover"
+                        alignItems="center"
+                      >
+                        <Typography sx={{ fontSize: 10 }} variant="span" component="div">
+                          {` link: ${index + 1}`}
+                        </Typography>
+                      </ListItemButton>
+                    </Button>
+                  </Grid>
+                </ListItem>
+              </List>
+            ))}
+        </Grid>
+        <Grid container justifyContent="center" alignItems="center" sx={{ marginBottom: '13px', marginTop: '13px' }}>
+          <Button
+            type="button"
+            onClick={() => {
+              const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+              const findSomeFavorite = favorites.some((favorite) => favorite._id === res._id);
+              if (findSomeFavorite) {
+                const filteredFavorites = favorites.filter(({ _id }) => _id !== res._id);
+                localStorage.setItem('favorites', JSON.stringify(filteredFavorites));
+              } else {
+                saveHistorySearch('favorites', res);
+              }
+            }}
+            size="small"
+            variant="contained"
+          >
+            <FavoriteIcon />
+          </Button>
+        </Grid>
       </Grid>
     </Grid>
+
   );
 }
 
