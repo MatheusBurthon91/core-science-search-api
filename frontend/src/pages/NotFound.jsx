62deb6a664f92231f900ddf33b19f7ab8b58fc66
Matheus Burthon <matatiaspatrizio@hotmail.com>
2022-09-14 01:12:15 -0300

    style: :lipstick: estilizando pagina de não encontrado.
    ainda falta providenciar centralizar elementos e colocar umas cores.

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { CardContent, Grid } from '@mui/material';
+import DoNotStepIcon from '@mui/icons-material/DoNotStep';
 
 export default function NotFound() {
+  const history = useHistory();
   return (
-    <main>
-      <h1>Você se perdeu retorne para a pagina inicial.</h1>
-      <Link to="/">Inicio</Link>
-    </main>
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      justify="center"
+      margin="10px 0px"
+    >
+      <Card>
+        <CardContent>
+          <Typography sx={{ fontSize: 25, color: 'blue' }} variant="h1" component="div">
+            Você se perdeu retorne para a pagina inicial.
+          </Typography>
+          <DoNotStepIcon sx={{
+            padding: '10px', fontSize: '100px',
+          }}
+          />
+          <CardActions>
+            <Button
+              size="small"
+              variant="contained"
+              onClick={() => history.push('/')}
+            >
+              Home
+            </Button>
+          </CardActions>
+
+        </CardContent>
+      </Card>
+    </Grid>
   );
 }
