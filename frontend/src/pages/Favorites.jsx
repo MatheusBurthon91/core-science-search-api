bdf6ea74b094b36b537e9dedbfd175a4feb06016
Matheus Burthon <matatiaspatrizio@hotmail.com>
2022-09-13 23:26:06 -0300

    style: :lipstick: adicioando estilo na pagina de favoritos, ainda passivel a mudanças.

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,13 +1,18 @@
+import { Grid, Typography } from '@mui/material';
 import React from 'react';
 import GetFavorites from '../components/getFavorites';
 import NavBar from '../components/NavBar';
 
 export default function Favorites() {
   return (
-    <main>
-      <h1 data-testid="favorite-title">listagem de favoritos.</h1>
+    <Grid container justifyContent="center">
+      <Grid container justifyContent="center" sx={{ backgroundColor: '#607EAA' }}>
+        <Typography variant="h1" component="div" gutterBottom data-testid="favorite-title">
+          listagem de favoritos.
+        </Typography>
+      </Grid>
       <NavBar barValue={1} />
       <GetFavorites />
-    </main>
+    </Grid>
   );
 }
